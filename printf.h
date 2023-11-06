c2b6a406f3b4dcae30048784f7adc28b78d0c0d4
Anton Kriese <anton.kriese@gmx.de>
2023-11-06 16:28:13 +0100

    fmt: remove print_ln function as it's not used

--- printf.h
+++ printf.h
@@ -1,9 +1,2 @@
 // prints a string with format identifiers and variable arguments
 void printf(const char *fmt, ...);
-
-// prints a string with format identifiers and variable arguments
-// additionally appends a new line to write a line
-void print_ln(const char *fmt, ...) {
-  // print(fmt, ...);
-  printf("\n\r");
-}
