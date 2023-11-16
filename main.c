59156f9c34f62e07939c62ba3fe9f446ccaf95bb
Anton Kriese <anton.kriese@gmx.de>
2023-11-16 17:23:30 +0100

    feat: map exception handler dummies to _start

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include "printf.h"
 #include "serial.h"
 
+extern int _perform_reset(void);
+extern int _perform_int(void);
+
 int main() {
   char *intro = "Type a character and shrineOS will translate it!";
   printf("%s (string stored at %x)\n\r", intro, intro);
@@ -11,6 +14,15 @@ int main() {
     char c = serial_read();
     printf("You gave the char: %c (%x)\n\r", c, c);
 
+    if (c == 'Q') {
+      printf("'Performing' a system reset...\n\r\n\r");
+      _perform_reset();
+    }
+
+    if (c == 'I') {
+      _perform_int();
+    }
+
     if (c == 'q') {
       printf("And now, I'll terminate myself...\n\r");
       break;
@@ -19,5 +31,10 @@ int main() {
     printf("shrineOS says: %c\n\r", c + 1);
   }
 
+  return 0;
+}
+
+int test_print(void) {
+  printf("Performed an interrupt!\n\r");
   return 0;
 }
