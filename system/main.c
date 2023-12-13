26ad7bcc2bb4becf3ec5a1acaca720e311157abc
Anton Kriese <anton.kriese@gmx.de>
2023-12-14 00:46:50 +0100

    feat(main): program to test interrupts
    not working properly at this point...
    might have to enable interrupts in the cpu in the first place

--- system/main.c
+++ system/main.c
@@ -1,11 +1,38 @@
 // include what you want to execute
 #include <dbgu.h>
 #include <print.h>
+#include <system.h>
 
 extern int _perform_reset(void);
 extern int _perform_int(void);
 
-int main() {
+extern void *check_stack_pointer(enum cpu_mode mode);
+
+int interrupt_program() {
+  print("start of the program\n\r");
+  dbgu_enable_interrupt();
+  st_activate_pits((unsigned char)1000);
+  print("IRQ stack pointer: %p\n\r", check_stack_pointer(CPU_MODE_IRQ));
+  print("SYS stack pointer: %p\n\r", check_stack_pointer(CPU_MODE_SYS));
+  print("UND stack pointer: %p\n\r", check_stack_pointer(CPU_MODE_UND));
+
+  install_interrupt_handlers();
+
+  print("setup done!\n\r");
+
+  while (1) {
+    dbgu_getc();
+
+    volatile int i; // volatile so it is not optimized away
+    for (i = 0; i < 10000; i++) {
+      // do nothing
+    }
+  }
+
+  return 0;
+}
+
+int exception_program() {
   char *intro =
       "Welcome to shrineOS! Type a character and shrineOS will respond.";
   print("\n\r%s (string stored at %x)\n\r", intro, intro);
@@ -59,3 +86,5 @@ int main() {
 
   return 0;
 }
+
+int main() { return interrupt_program(); }
