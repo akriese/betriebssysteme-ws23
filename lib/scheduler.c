4edd40a05e1f61bbf14ff9feffff57b4b8a6c9dc
Anton Kriese <anton.kriese@gmx.de>
2023-12-18 16:14:32 +0100

    feat(thread): thread management object

--- lib/scheduler.c
+++ lib/scheduler.c
@@ -1,3 +1,9 @@
+#include <mem_layout.h>
+#include <scheduler.h>
+#include <thread.h>
+
+volatile struct thread_management *const thread_management =
+    (struct thread_management *)_INTERNAL_THREADS_MANAGEMENT_START;
 
 void scheduler_register_thread();
 
--- lib/thread.c
+++ lib/thread.c
@@ -1,5 +1,13 @@
+#include <mem.h>
+#include <mem_layout.h>
+#include <scheduler.h>
+#include <thread.h>
 
+volatile struct thread_control_block *const tcbs =
+    (struct thread_control_block *)_INTERNAL_THREADS_TCB_ARRAY_START;
 
+volatile struct thread_management *const management =
+    (struct thread_management *)_INTERNAL_THREADS_MANAGEMENT_START;
 /**
  * @brief Creates a new thread with which runs the given function.
  *
