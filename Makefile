452cede5b204875e67c123afe1294a9aece0260c
Anton Kriese <anton.kriese@gmail.com>
2023-12-23 10:07:43 +0100

    feat: set exit thread as LR of new thread

--- Makefile
+++ Makefile
@@ -20,6 +20,7 @@ LSCRIPT = kernel.lds
 OBJ = driver/dbgu.o
 OBJ += lib/print.o lib/util.o lib/mem.o lib/thread.o lib/scheduler.o
 OBJ += lib/programs/dbgu.o lib/programs/exceptions.o lib/programs/interrupts.o lib/programs/threads.o
+OBJ += lib/sys_call.o
 
 OBJ += system/main.o system/start.o system/memory_controller.o system/exceptions.o
 OBJ += system/timer.o system/interrupt.o
--- lib/thread.c
+++ lib/thread.c
@@ -2,6 +2,7 @@
 #include <mem_layout.h>
 #include <print.h>
 #include <scheduler.h>
+#include <sys_call.h>
 #include <system.h>
 #include <thread.h>
 
@@ -10,6 +11,28 @@ volatile struct thread_control_block *const tcbs =
 
 volatile struct thread_management *const management =
     (struct thread_management *)_INTERNAL_THREADS_MANAGEMENT_START;
+
+void __create_thread(unsigned int id, int (*fun)(void *), void *input) {
+  struct thread_control_block *new_tcb = &tcbs[id];
+
+  // reset tcb content to get rid off previous content
+  memset(new_tcb, 0, sizeof(struct thread_management));
+
+  new_tcb->sp = _INTERNAL_THREADS_STACKS_START +
+                THREAD_STACK_SIZE * id; // TODO: find good start for stack
+  new_tcb->lr = (unsigned int)sys_call_exit_thread;
+  new_tcb->pc = (unsigned int)fun;
+  new_tcb->cpsr = CPU_MODE_USER; // TODO: find useful default value for cpsr
+  new_tcb->registers[0] = (unsigned int)input;
+
+  management->last_created_id = id;
+
+  print("Thread with id %d created!\n\r", id);
+  // print_context(new_tcb);
+  // register_dump(new_tcb + 4);
+  print("%X", new_tcb->registers[0]);
+}
+
 /**
  * @brief Creates a new thread with which runs the given function.
  *
@@ -28,26 +51,9 @@ int thread_create(int (*fun)(void *), void *input) {
     return 1;
   }
 
-  struct thread_control_block *new_tcb = &tcbs[thread_id];
-
-  // reset tcb content to get rid off previous content
-  memset(new_tcb, 0, sizeof(struct thread_management));
-
-  new_tcb->sp =
-      _INTERNAL_THREADS_STACKS_START +
-      THREAD_STACK_SIZE * thread_id; // TODO: find good start for stack
-  new_tcb->lr = (unsigned int)thread_finish;
-  new_tcb->pc = (unsigned int)fun;
-  new_tcb->cpsr = CPU_MODE_USER; // TODO: find useful default value for cpsr
-  new_tcb->registers[0] = (unsigned int)input;
-
-  management->last_created_id = thread_id;
+  __create_thread(thread_id, fun, input);
 
   scheduler_register_thread(thread_id);
-  print("Thread with id %d created!\n\r", thread_id);
-  // print_context(new_tcb);
-  // register_dump(new_tcb + 4);
-  print("%X", new_tcb->registers[0]);
 
   return 0;
 }
@@ -55,39 +61,22 @@ int thread_create(int (*fun)(void *), void *input) {
 void thread_finish() {
   unsigned int finished_thread_id = management->active_thread_id;
   management->status[finished_thread_id] = TCB_UNUSED;
-
-  scheduler_next(0);
+  print("Thread %d finished!\n\r", finished_thread_id);
 }
 
 void thread_save_context(unsigned int thread_id, unsigned int *context) {
   struct thread_control_block *tcb = &tcbs[thread_id];
 
   memcpy(context, (void *)tcb, 17 * 4);
-  // tcb->cpsr;
 }
 
 void *thread_get_context(unsigned int thread_id) { return &(tcbs[thread_id]); }
 
 void create_idle_thread(int (*idle_fun)()) {
-  unsigned int thread_id = MAX_NUM_THREADS - 1;
+  unsigned int id = MAX_NUM_THREADS - 1;
+  __create_thread(id, idle_fun, 0);
 
-  struct thread_control_block *new_tcb = &tcbs[thread_id];
-
-  // reset tcb content to get rid off previous content
-  memset(new_tcb, 0, sizeof(struct thread_management));
-
-  new_tcb->sp =
-      _INTERNAL_THREADS_STACKS_START +
-      THREAD_STACK_SIZE * thread_id; // TODO: find good start for stack
-  new_tcb->lr = (unsigned int)thread_finish;
-  new_tcb->pc = (unsigned int)idle_fun;
-  new_tcb->cpsr = CPU_MODE_USER; // TODO: find useful default value for cpsr
-
-  management->status[thread_id] = IDLE;
-  // print("Idle thread's context (%p), tcb (%p), last tcb (%p): ", new_tcb,
-  // tcbs,
-  //       &tcbs[thread_id]);
-  // print_context(new_tcb);
+  management->status[id] = IDLE;
 }
 
 void print_context(void *context) {
