268d461ad79cc38fd001b4a92f8dd8893a08e79d
Anton Kriese <anton.kriese@gmx.de>
2024-01-04 17:28:57 +0100

    fix: mark clobbered lr register in inline assembly

--- lib/sys_call.c
+++ lib/sys_call.c
@@ -12,15 +12,16 @@
 // If input and output are given, other macros are used.
 // This only allows for one input and one ouput each, as otherwise the macro
 // has to be more complex
-#define __SWI(NUM) __asm__ volatile("swi %0" : : "I"(NUM))
+#define __SWI(NUM) __asm__ volatile("swi %0" : : "I"(NUM) : "r14")
 #define __SWI_IN(NUM, INPUT)                                                   \
-  __asm__ volatile("mov r0, %1\n\tswi %0" : : "I"(NUM), INPUT)
+  __asm__ volatile("mov r0, %1\n\tswi %0" : : "I"(NUM), INPUT : "r14")
 #define __SWI_IN_OUT(NUM, INPUT, OUTPUT)                                       \
   __asm__ volatile("mov r0, %2\n\tswi %1\n\tmov %0, r0"                        \
                    : OUTPUT                                                    \
-                   : "I"(NUM), INPUT)
+                   : "I"(NUM), INPUT                                           \
+                   : "r14")
 #define __SWI_OUT(NUM, OUTPUT)                                                 \
-  __asm__ volatile("swi %1\n\tmov %0, r0" : OUTPUT : "I"(NUM))
+  __asm__ volatile("swi %1\n\tmov %0, r0" : OUTPUT : "I"(NUM) : "r14")
 #define __GET_SWI_MACRO(_1, _2, _3, NAME, ...) NAME
 #define NUMBERED_SWI(...)                                                      \
   __GET_SWI_MACRO(__VA_ARGS__, __SWI_IN_OUT, __SWI_IN, __SWI)(__VA_ARGS__)
@@ -86,9 +87,13 @@ void sys_call_exit_thread() { NUMBERED_SWI(SYSCALL_NUM_THREAD_EXIT); }
 
 int sys_call_create_thread(int (*fun)(void *), void *input) {
   int result;
-  __asm__ volatile("mov r0, %2\n\tmov r1, %3\n\tswi %1\n\tmov %0, r0"
+  __asm__ volatile("mov r0, %2\n\t"
+                   "mov r1, %3\n\t"
+                   "swi %1\n\t"
+                   "mov %0, r0"
                    : "=r"(result)
-                   : "I"(SYSCALL_NUM_THREAD_CREATE), "r"(fun), "r"(input));
+                   : "I"(SYSCALL_NUM_THREAD_CREATE), "r"(fun), "r"(input)
+                   : "r14");
   return result;
 }
 
